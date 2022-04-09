9e8165a9d3970e2f3ca1d62daae83ebb57336de5
Jingsong Gao <jg2109@georgetown.edu>
2022-04-09 12:42:12 -0400

    Fix edge display

--- data_for_viz/04-beeswarm/beeswarm_d3.js
+++ data_for_viz/04-beeswarm/beeswarm_d3.js
@@ -4,9 +4,10 @@ height = 920
 svg.attr("height", height)
   .attr("width", width);
 
-console.log(JSON.stringify(data.links[1]))
+const nodes = data.nodes.map(d => Object.create(d));
+const links = data.links.map(d => Object.create(d));
 
-let sectors = Array.from(new Set(data.nodes.map((d) => d.nutrition)));
+let sectors = Array.from(new Set(nodes.map((d) => d.nutrition)));
 let xScale = d3
       .scalePoint()
       .domain(sectors)
@@ -20,31 +21,34 @@ svg.append("g")
   .call(d3.axisBottom(xAxis))
 let yScale = d3
       .scaleLinear()
-      .domain(d3.extent(data.nodes.map((d) => +d.value_norm)))
+      .domain(d3.extent(nodes.map((d) => +d.value_norm)))
       .range([1/8 * height+margin[1], 6/8 * height+margin[1]]);
       
 let color = d3.scaleOrdinal().domain(sectors).range(d3.schemePaired);
 
-let valueDomain = d3.extent(data.nodes.map((d) => +d.value));
+let valueDomain = d3.extent(nodes.map((d) => +d.value));
 let size = d3.scaleSqrt().domain(valueDomain).range([3, 8]);
 
-var link = svg.append("g")
+const link = svg.append("g")
     .selectAll("line")
-    .data(data.links)
-    .enter().append("line")
-    .style("stroke", "gray")
-    .attr("stroke-width", (d) => d.value);
+    .data(links)
+    .enter()
+    .append("line")
+    .style("stroke", "grey")
+    .style('stroke-width', 0.1)
+    .style("stroke-opacity", 0.5);
 
 const node = svg.append("g")
     .selectAll(".circ")
-    .data(data.nodes)
+    .data(nodes)
     .enter()
     .append("circle")
     .attr("class", "circ")
     .attr("fill", (d) => color(d.type))
     .attr("r", (d) => size(d.value));
 
-let simulation = d3.forceSimulation(data.nodes)
+let simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(d => d.id).strength(0.01))
     .force("x", d3.forceX((d) => {
         return xScale(d.nutrition);
         }).strength(3))
@@ -54,7 +58,13 @@ let simulation = d3.forceSimulation(data.nodes)
     .force("collide", d3.forceCollide((d) => {
         return 1.5 * size(d.value);
         }))
+
+simulation
+    .nodes(nodes)
     .on("tick", tick);
+
+simulation.force("link")
+    .links(links);
     
 function tick() {
       node

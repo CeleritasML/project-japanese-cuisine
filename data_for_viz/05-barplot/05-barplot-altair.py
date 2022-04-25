dd36716d2383b107779fe78f6bba0fad92aad59d
Jingsong Gao <jg2109@georgetown.edu>
2022-04-25 10:39:25 -0400

    Clean up code

--- data_for_viz/05-barplot/05-barplot-altair.py
+++ data_for_viz/05-barplot/05-barplot-altair.py
@@ -3,37 +3,31 @@ This file generates the interactive visualization for 05-barplot.
 """
 
 import altair as alt
-import numpy as np
 import pandas as pd
-import os
 
 freq = pd.read_csv('ingredients-by-freq.csv')
 
-freq.category = pd.Categorical(freq.category, ordered = True) # make categories an ordered factor
-
+freq.category = pd.Categorical(freq.category, ordered=True)  # make categories an ordered factor
 
 # configuring dropdown bar
 dropdown_selection = alt.binding_select(options=['appetizer',
-                                             'breakfast',
-                                             'dessert',
-                                             'entree',
-                                             'salad',
-                                             'side'], 
-                                    name='Recipe Category: ')
+                                                 'breakfast',
+                                                 'dessert',
+                                                 'entree',
+                                                 'salad',
+                                                 'side'],
+                                        name='Recipe Category: ')
 
 dropdown_selection_single = alt.selection_single(fields=['category'], bind=dropdown_selection)
-
 
 alt.renderers.enable('altair_viewer')
 
 alt.Chart(freq).mark_bar().encode(
-    x = alt.X("name:O", sort='-y'),
-    y = 'freq:Q',
-    color = 'origin:N'
-).transform_filter( # apply filter by dropdown
+    x=alt.X("name:O", sort='-y'),
+    y='freq:Q',
+    color='origin:N'
+).transform_filter(  # apply filter by dropdown
     dropdown_selection_single
-).add_selection( # add dropdown selection
+).add_selection(  # add dropdown selection
     dropdown_selection_single
 ).interactive()
-
-
--- data_for_viz/05-barplot/viz.R
+++ data_for_viz/05-barplot/viz.R
@@ -1,6 +1,6 @@
-"""
-This file generates the fifth visualization 05-barplot. 
-"""
+###
+### This file generates the fifth visualization 05-barplot.
+###
 
 library(tidyverse)
 library(plotly)
@@ -8,22 +8,22 @@ library(htmlwidgets)
 
 
 setwd('./data_for_viz/05-barplot/')
-dat1 = read_csv('ingredients-by-freq.csv')
-dat2 = read_csv('japan-timelines.csv')
+dat1 <- read_csv('ingredients-by-freq.csv')
+dat2 <- read_csv('japan-timelines.csv')
 
-dat2 = dat2 |> 
+dat2 <- dat2 |>
   select(-Week) |>
   pivot_longer(cols = everything(),
                names_to = 'name',
                values_to = 'clicks')
 
 dat1$name <- reorder(dat1$name, dat1$freq)
-dat2$name = factor(dat2$name, levels = levels(dat1$name))
+dat2$name <- factor(dat2$name, levels = levels(dat1$name))
 
 fig1 <- dat1 |> plot_ly(x = ~name, y = ~freq, type = 'bar', name = ~origin) 
 fig2 <- dat2 |> plot_ly(x = ~name, y = ~clicks, type = "box")
 
-fig <- subplot(fig1, fig2, nrows = 2, shareX = TRUE) %>% 
+fig <- subplot(fig1, fig2, nrows = 2, shareX = TRUE) %>%
   layout(title = list(text = "What are the most used ingredients in Japanese entree recipes?"),
          plot_bgcolor='#e5ecf6', 
          xaxis = list( 

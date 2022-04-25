5f365654f95ebb205337d61873ebc833b110aca0
ErcongLuo <luoercong@gmail.com>
2022-04-25 01:39:14 -0400

    visualization sketch is out, need to further refine the plot

--- data_for_viz/05-barplot/viz.R
+++ data_for_viz/05-barplot/viz.R
@@ -9,25 +9,36 @@ library(htmlwidgets)
 
 setwd('./data_for_viz/05-barplot/')
 dat1 = read_csv('ingredients-by-freq.csv')
-dat2 = read_csv('google-trends-popularity.csv')
+dat2 = read_csv('japan-timelines.csv')
 
-dat1$origin = as.factor(dat1$origin)
+dat2 = dat2 |> 
+  select(-Week) |>
+  pivot_longer(cols = everything(),
+               names_to = 'name',
+               values_to = 'clicks')
 
-dat1$name = as.factor(dat1$name)
-dat1$name = reorder(dat1$name, dat1$freq)
+dat1$name <- reorder(dat1$name, dat1$freq)
+dat2$name = factor(dat2$name, levels = levels(dat1$name))
 
-f1 = dat1 |>
-  plot_ly(
-        x = ~name,
-        y = ~freq,
-        type = 'bar',
-        name = ~origin
-        ) |>
-  layout(title = 'What are the most used ingredients in Japanese entree recipes?',
-         xaxis = list(title = 'Name of recipe'),
-         yaxis = list(title = 'Number of Occurences in Entree Recipes'))
+fig1 <- dat1 |> plot_ly(x = ~name, y = ~freq, type = 'bar', name = ~origin) 
+fig2 <- dat2 |> plot_ly(x = ~name, y = ~clicks, type = "box")
 
-saveWidget(f1, "05-barplot.html", selfcontained = T, libdir = "lib")
+fig <- subplot(fig1, fig2, nrows = 2, shareX = TRUE) %>% 
+  layout(title = list(text = "What are the most used ingredients in Japanese entree recipes?"),
+         plot_bgcolor='#e5ecf6', 
+         xaxis = list( 
+           zerolinecolor = '#ffff', 
+           zerolinewidth = 2, 
+           gridcolor = 'ffff'), 
+         yaxis = list( 
+           zerolinecolor = '#ffff', 
+           zerolinewidth = 2, 
+           gridcolor = 'ffff')) 
+fig
+
+
+
+saveWidget(fig, "05-barplot.html", selfcontained = T, libdir = "lib")
 
 ### TODO: 
 # dat2 contains time-series data on each of the ingredients
